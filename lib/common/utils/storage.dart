c0b0085a79ba5eae7e6f3143fd75268ee35dbd57
YeCaiYu <yecaiyu@foxmail.com>
2023-10-24 14:59:22 +0800

    storage kv离线存储

---
+++ lib/common/utils/storage.dart
@@ -0,0 +1,43 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+//kv离线存储
+class Storage {
+  //单例写法
+  static final Storage _instance = Storage._internal();
+  factory Storage() => _instance;
+  late final SharedPreferences _prefs;
+
+  Storage._internal();
+
+  Future<void> init() async {
+    _prefs = await SharedPreferences.getInstance();
+  }
+
+  Future<void> setString(String key, String value) async {
+    _prefs.setString(key, value);
+  }
+
+  Future<void> setBool(String key, bool value) async {
+    _prefs.setBool(key, value);
+  }
+
+  Future<void> setList(String key, List<String> value) async {
+    _prefs.setStringList(key, value);
+  }
+
+  String getString(String key) {
+    return _prefs.getString(key) ?? '';
+  }
+
+  bool getBool(String key) {
+    return _prefs.getBool(key) ?? false;
+  }
+
+  List<String> getList(String key) {
+    return _prefs.getStringList(key) ?? [];
+  }
+
+  Future<bool> remove(String key) async {
+    return await _prefs.remove(key);
+  }
+}
--- lib/global.dart
+++ lib/global.dart
@@ -7,6 +7,9 @@ class Global {
   static Future<void> init() async {
     WidgetsFlutterBinding.ensureInitialized();
 
+    //工具类
+    await Storage().init();
+
     Get.put<ConfigService>(ConfigService());
   }
 }
