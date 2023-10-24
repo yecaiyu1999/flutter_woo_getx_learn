9853d9329a00bc230990e65368bc2a0cc64d2c13
YeCaiYu <yecaiyu@foxmail.com>
2023-10-24 09:24:41 +0800

    项目初始化

---
+++ android/app/src/main/kotlin/woo/yecaiyu/cn/flutter_woo_getx_learn/MainActivity.kt
@@ -0,0 +1,6 @@
+package woo.yecaiyu.cn.flutter_woo_getx_learn
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'common/index.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: RouteNames.systemSplash,
+      getPages: RoutePages.list,
+      navigatorObservers: [RoutePages.observer],
+    );
+  }
+}
