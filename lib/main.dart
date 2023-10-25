8c08f24de0580bf6f208a90773bcf5af5f360c62
YeCaiYu <yecaiyu@foxmail.com>
2023-10-25 09:45:40 +0800

    根据设计稿自动适配屏幕

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
 import 'common/index.dart';
@@ -15,19 +16,39 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'Flutter Demo',
-      theme: ConfigService.to.isDarkModel ? AppTheme.dark : AppTheme.light,
-      initialRoute: RouteNames.stylesStylesIndex,
-      getPages: RoutePages.list,
-      navigatorObservers: [RoutePages.observer],
+    return ScreenUtilInit(
+      designSize: const Size(414, 896), // 设计稿中设备的尺寸(单位随意,建议dp,但在使用过程中必须保持一致)
+      splitScreenMode: false, // 支持分屏尺寸
+      minTextAdapt: false, // 是否根据宽度/高度中的最小值适配文字
+      // 一般返回一个MaterialApp类型的Function()
+      builder: (context, child) {
+        return GetMaterialApp(
+          title: 'Flutter Demo',
+          theme: ConfigService.to.isDarkModel ? AppTheme.dark : AppTheme.light,
 
-      //多语言
-      translations: Translation(), //词典
-      localizationsDelegates: Translation.localizationsDelegates, //代理
-      supportedLocales: Translation.supportedLocales, //支持的语言种类
-      locale: ConfigService.to.locale, //当前语言种类
-      fallbackLocale: Translation.fallbackLocale, //默认语言种类
+          //路由
+          initialRoute: RouteNames.stylesStylesIndex,
+          getPages: RoutePages.list,
+          navigatorObservers: [RoutePages.observer],
+
+          //多语言
+          translations: Translation(), //词典
+          localizationsDelegates: Translation.localizationsDelegates, //代理
+          supportedLocales: Translation.supportedLocales, //支持的语言种类
+          locale: ConfigService.to.locale, //当前语言种类
+          fallbackLocale: Translation.fallbackLocale, //默认语言种类
+
+          builder: (context, widget) {
+            // 不随系统字体缩放比例
+            return MediaQuery(
+              data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+              child: widget!,
+            );
+          },
+
+          debugShowCheckedModeBanner: false,
+        );
+      },
     );
   }
 }
